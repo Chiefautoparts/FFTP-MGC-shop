55337c01a1f3d6c9268a84bce7cce96fd38009e9
chiefautoparts <chiefautoparts@outlook.com>
2017-10-17 03:00:53 -0700

    framework for online shop complete

--- myshop/__init__.py
+++ myshop/__init__.py
@@ -1,2 +1,3 @@
-# import celery
-from .celery import app as celery_app
+# from __future__ import absolute_import
+# # import celery
+# from .celery import app as celery_app
--- orders/tasks.py
+++ orders/tasks.py
@@ -1,16 +1,16 @@
-from celery import Celery
-from django.core.mail import send_email
-from .models import Order
+# from celery import task
+# from django.core.mail import send_email
+# from .models import Order
 
-app = Celery('tasks', broker='amqp://localhost//')
+# app = Celery('tasks', broker='amqp://localhost//')
 
-@app.task
-def order_created(order_id):
-	"""
-	Task to send an e-mail notification when an order is successfully created
-	"""
-	order = Order.objects.get(id=order_id)
-	subject = 'Order nr. {}'.format(order.id)
-	message = 'Dear {}, \n\nYou have successfully placed an order. Your order id is {}.'.format(order.first_name, order.id)
-	mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
-	return mail_sent
+# @app.task
+# def order_created(order_id):
+# 	"""
+# 	Task to send an e-mail notification when an order is successfully created
+# 	"""
+# 	order = Order.objects.get(id=order_id)
+# 	subject = 'Order nr. {}'.format(order.id)
+# 	message = 'Dear {}, \n\nYou have successfully placed an order. Your order id is {}.'.format(order.first_name, order.id)
+# 	mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email], fail_silently=False)
+# 	return mail_sent
