6c1dbf325070dd1b1e076c08302a363d9551786f
chiefautoparts <chiefautoparts@outlook.com>
2017-10-20 22:56:55 -0700

    added coupons featrure

---
+++ coupons/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+from .models import Coupon
+from .forms import CouponApplyForm
+
+@require_POST
+def coupon_apply(request):
+	now = timezone.now()
+	form = CouponApplyForm(request)
+	if form .is_valid():
+		code = form.cleaned_data['code']
+		try:
+			coupon = Coupon.objects.get(code_iexact=code, valid_form__lte=now, valid_to__gte=now,activate=True)
+			request.session['coupon_id'] = coupon.id
+		except Coupon.DoesNotExist:
+			request.session['coupon_id'] None
+	return redirect('cart:cart_detail')
